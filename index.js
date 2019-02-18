472350b79d3606b383733cd2eca49e44969953be
AshVivier <ash.set.vivier@gmail.com>
2019-02-18 13:38:36 +1300

    typo

---
+++ index.js
@@ -0,0 +1,17 @@
+
+MakeSandichWith _________
+    get a piece of Bread;
+    put _________ on top;
+    put another piece of bread on top of that;
+    
+
+function MakeSandichWith (filling) {
+get a piece of Bread;
+put filling on top;
+put another piece of bread on top of that;
+}
+
+MakeSandichWith(ham)
+
+
+
